128958e6f0b86ab09b4c7ab5f12e4a54248dc98c
msakuta <masahiro.sakuta@gmail.com>
2023-04-16 06:46:22 +0900

    Publish TreeSource for crate users

--- src/parser.rs
+++ src/parser.rs
@@ -4,6 +4,6 @@ mod yaml_parser;
 
 pub use self::{
     loader::load,
-    nom_parser::{node_def, parse_file, parse_nodes, BlackboardValue, NodeDef, TreeDef, PortMap, PortMapOwned},
+    nom_parser::{node_def, parse_file, parse_nodes, BlackboardValue, NodeDef, TreeDef, PortMap, PortMapOwned, TreeSource},
     yaml_parser::load_yaml,
 };
